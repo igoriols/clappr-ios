2e2a61ad31574347dcef7a4177008844cbfe17ef
Carlos Corrêa <caiobzen@gmail.com>
2019-07-10 16:09:34 -0300

    feat: introduce backgroundPlugin

--- Sources/Clappr_iOS/Classes/Base/Player.swift
+++ Sources/Clappr_iOS/Classes/Base/Player.swift
@@ -192,6 +192,7 @@ open class Player: BaseObject {
     open class func register(plugins: [Plugin.Type]) {
         if !hasAlreadyRegisteredPlugins {
             let builtInPlugins: [Plugin.Type] = [
+                BackgroundPlugin.self,
                 MediaControl.self,
                 PosterPlugin.self,
                 SpinnerPlugin.self,
